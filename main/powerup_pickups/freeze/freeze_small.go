1b8af97a054d3a0b06a8b7ea1ecca1c967dbf235
Tyler <tiller1010@gmail.com>
2025-07-15 08:51:25 -0400

    feat: powerup ammo count

---
+++ main/powerup_pickups/freeze/freeze_small.go
@@ -0,0 +1,49 @@
+components {
+  id: "freeze"
+  component: "/main/powerup_pickups/freeze/freeze.script"
+  properties {
+    id: "ammo_amount"
+    value: "1.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/powerup_pickups/freeze/freeze.tilesource\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.5
+    y: 0.5
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 21.0\n"
+  "  data: 21.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
