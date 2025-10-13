00b326121be3976adcac3644f5ca3869fe7f1786
Tyler <tiller1010@gmail.com>
2025-10-13 08:51:22 -0400

    fix: do not play menu music more than once, do not navigate to locked level buttons

--- main/gui_controls.lua
+++ main/gui_controls.lua
@@ -6,22 +6,49 @@ local NORMAL_COLOR = vmath.vector4(1, 0, 0, 1)
 
 function update_highlight(self, gui)
 	for i, node in ipairs(self.buttons) do
-		if i == self.selected_index then
+		local button_unlocked = true
+
+		if self.levels_data then
+			local level_data = self.levels_data['level_' .. i]
+			if level_data then
+				button_unlocked = level_data.unlocked
+			end
+		end
+		
+		if i == self.selected_index and button_unlocked then
 			gui.set_scale(node, HIGHTLIGHT_SCALE)
 			gui.set_color(node, HIGHTLIGHT_COLOR)
 		else
 			gui.set_scale(node, NORMAL_SCALE)
 			gui.set_color(node, NORMAL_COLOR)
+			
+			if not button_unlocked then
+				gui.set_alpha(node, .5)
+			end
 		end
+		
 	end
 end
 
 return {
 	navigate = function(self, delta, gui)
-		local buttons_count = #self.buttons
-		self.selected_index = self.selected_index + delta
-		if self.selected_index < 1 then self.selected_index = buttons_count end
-		if self.selected_index > buttons_count then self.selected_index = 1 end
-		update_highlight(self, gui)
+		local new_selected_index = self.selected_index + delta
+		
+		local button_unlocked = true
+
+		if self.levels_data then
+			local level_data = self.levels_data['level_' .. new_selected_index]
+			if level_data then
+				button_unlocked = level_data.unlocked
+			end
+		end
+
+		if button_unlocked then
+			self.selected_index = new_selected_index
+			local buttons_count = #self.buttons
+			if self.selected_index < 1 then self.selected_index = buttons_count end
+			if self.selected_index > buttons_count then self.selected_index = 1 end
+			update_highlight(self, gui)
+		end
 	end
 }
